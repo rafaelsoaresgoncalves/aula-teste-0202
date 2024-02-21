93ab17a402d5a0b11f0dd072a101bf3eb76f6c4e
rafaelsoaresgoncalves <133134742+rafaelsoaresgoncalves@users.noreply.github.com>
2024-02-21 11:47:23 -0300

    Add files via upload

---
+++ teste solo/app.js
@@ -0,0 +1,9 @@
+const meuBotao = document.getElementById('play');
+
+function nomedafuncao() {
+    alert('teste solo')
+    console.log("teste")
+};
+
+
+meuBotao.addEventListener('click', nomedafuncao);
